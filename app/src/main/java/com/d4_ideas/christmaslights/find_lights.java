9149f995a6127bb844b0a8b767e0c55756661ef5
Anthony Sheetz <sheetzam@earthlink.net>
2015-10-01 15:20:59 -0400

    Now with markers and geojson!

--- app/src/main/java/com/d4_ideas/christmaslights/find_lights.java
+++ app/src/main/java/com/d4_ideas/christmaslights/find_lights.java
@@ -11,12 +11,32 @@ import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.VisibleRegion;
+import com.google.maps.android.geojson.GeoJsonFeature;
+import com.google.maps.android.geojson.GeoJsonLayer;
+import com.google.maps.android.geojson.GeoJsonPointStyle;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -26,25 +46,31 @@ import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class find_lights extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private static final String DEBUG_TAG = "Christmas Lights";
+    public Location ourLocation;
+    private static final String POSTENDPOINT = "http://192.168.42.18/post.php";
+    private static final String GETENDPOINT = "http://192.168.42.18/get.php";
+    JSONArray markers = null;
+    private GeoJsonLayer mLayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(DEBUG_TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_lights);
         setUpMapIfNeeded();
     }
-
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
     }
-
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
@@ -61,6 +87,7 @@ public class find_lights extends FragmentActivity {
      * method in {@link #onResume()} to guarantee that it will be called.
      */
     private void setUpMapIfNeeded() {
+        Log.d(DEBUG_TAG, "setUpMapIfNeeded.");
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
@@ -72,12 +99,19 @@ public class find_lights extends FragmentActivity {
             }
         }
     }
-
     /**
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
+        Log.d(DEBUG_TAG, "starting setUpMap");
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                getMarkers();
+            }
+        });
 
         Criteria criteria = new Criteria();
         criteria.setAccuracy(Criteria.ACCURACY_FINE);
@@ -86,39 +120,123 @@ public class find_lights extends FragmentActivity {
 
         LocationListener locationListener = new LocationListener() {
             @Override
-            public void onLocationChanged(Location location) {
-                showCurrentLocation(location);
+            public void onLocationChanged(Location ourLocation) {
+                Log.d(DEBUG_TAG, "onLocationChanged.");
+                showCurrentLocation(ourLocation);
+                getMarkers();
             }
 
             @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
 
             @Override
-            public void onProviderEnabled(String provider) {
-
-            }
+            public void onProviderEnabled(String provider) {}
 
             @Override
-            public void onProviderDisabled(String provider) {
-
-            }
+            public void onProviderDisabled(String provider) {}
         };
 
         locationManager.requestLocationUpdates(provider, 2000, 0, locationListener);
 
-        Location location = locationManager.getLastKnownLocation(provider);
-        if (location != null) {
-            showCurrentLocation(location);
+        ourLocation = locationManager.getLastKnownLocation(provider);
+        if (ourLocation != null) {
+            showCurrentLocation(ourLocation);
         }
     }
+    private void getMarkers() {
+        Log.d(DEBUG_TAG, "getMarkers");
+        String urlParameters;
+        String url;
+        final VisibleRegion visibleRegion=mMap.getProjection().getVisibleRegion();
 
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (null == ourLocation){
+            Log.d(DEBUG_TAG, "We have no location!");
+        } else if (networkInfo != null && networkInfo.isConnected()) {
+            // fetch data
+            Log.d(DEBUG_TAG, "Looks like the network is available.");
+            Log.d(DEBUG_TAG, visibleRegion.latLngBounds.northeast.toString());
+            urlParameters =
+                       "N=" + visibleRegion.latLngBounds.northeast.latitude
+                    + "&W=" + visibleRegion.latLngBounds.northeast.longitude
+                    + "&S=" + visibleRegion.latLngBounds.southwest.latitude
+                    + "&E=" + visibleRegion.latLngBounds.southwest.longitude;
+            url = GETENDPOINT + "?" + urlParameters;
+            JsonObjectRequest jsonRequest = new JsonObjectRequest
+                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d(DEBUG_TAG, response.toString());
+                            mLayer = new GeoJsonLayer(getMap(), response);
+                            decorateMarkers();
+                            mLayer.addLayerToMap();
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            error.printStackTrace();
+                            Log.d(DEBUG_TAG, "something went wrong requesting json.");
+                        }
+                    });
+            Volley.newRequestQueue(this).add(jsonRequest);
+        } else {
+            Log.d(DEBUG_TAG, "Sorry, no network connection.");
+        }
+    }
+    public void upVote(View view) {
+        Log.d(DEBUG_TAG, "upVote");
+        vote("up");
+    }
+    public void downVote(View view) {
+        Log.d(DEBUG_TAG, "downVote");
+        vote("down");
+    }
+    private void vote(final String theVote){
+        Log.d(DEBUG_TAG, "Voting");
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (null == ourLocation){
+            Log.d(DEBUG_TAG, "We have no location!");
+        } else if (networkInfo != null && networkInfo.isConnected()) {
+            // fetch data
+            Log.d(DEBUG_TAG, "Looks like the network is available.");
+
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(this);
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, POSTENDPOINT,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            Log.d(DEBUG_TAG, "Response is: "+ response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d(DEBUG_TAG, "post did not work");
+                    Log.d(DEBUG_TAG, error.toString());
+                }
+            }){
+                protected Map<String, String> getParams() throws com.android.volley.AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("lat", String.valueOf(ourLocation.getLatitude()));
+                    params.put("long", String.valueOf(ourLocation.getLongitude()));
+                    params.put("vote", theVote);
+                    return params;
+                };
+            };
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+        } else {
+            Log.d(DEBUG_TAG, "Sorry, no network connection.");
+        }
+    }
     private void showCurrentLocation (Location location) {
-        // first update the map
-        String result = null;
-        String stringUrl = "http://192.168.42.18/post.php";
-        String urlParameters = "fName=foo&lName=bar";
+        Log.d(DEBUG_TAG, "showCurrentLocation");
         mMap.clear();
         LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
 
@@ -130,85 +248,31 @@ public class find_lights extends FragmentActivity {
         );
 
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 18));
-
-        //now update the server
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            // fetch data
-            Log.d(DEBUG_TAG, "Looks like the network is available.");
-            urlParameters = "lat=" + location.getLatitude() + "&long=" + location.getLongitude();
-            new DoPostTask().execute(stringUrl, urlParameters);
-        } else {
-            Log.d(DEBUG_TAG, "Sorry, no network connection.");
-        }
     }
+    protected GoogleMap getMap() {
+        setUpMapIfNeeded();
+        return mMap;
+    }
+    private void decorateMarkers() {
+        // Iterate over all the features stored in the layer
+        for (GeoJsonFeature feature : mLayer.getFeatures()) {
+            // Check if the magnitude property exists
+            if (feature.hasProperty("votes")) {
+                float votes = Float.parseFloat(feature.getProperty("votes"));
 
-    private class DoPostTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls){
-            try {
-                return postUrl(urls[0], urls[1]);
-            } catch (IOException e) {
-                return "Unable to post to web page.";
+                // Get the icon for the feature
+                BitmapDescriptor pointIcon = BitmapDescriptorFactory.defaultMarker();
+
+                // Create a new point style
+                GeoJsonPointStyle pointStyle = new GeoJsonPointStyle();
+
+                // Set options for the point style
+                pointStyle.setIcon(pointIcon);
+                pointStyle.setAlpha(votes);
+
+                // Assign the point style to the feature
+                feature.setPointStyle(pointStyle);
             }
         }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Log.d(DEBUG_TAG, result);
-        }
-    }
-
-    private String postUrl(String myurl, String urlParameters) throws IOException {
-        InputStream is = null;
-        // Only display the first 500 characters of the retrieved
-        // web page content.
-        int len = 500;
-
-        try {
-            URL url = new URL(myurl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("POST");
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-            conn.setUseCaches(false);
-            conn.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
-            conn.setRequestProperty("Content-Language", "en-US");
-
-            //Send Request
-            DataOutputStream wr = new DataOutputStream (
-                    conn.getOutputStream ());
-            wr.writeBytes(urlParameters);
-            wr.flush();
-            wr.close();
-            // Starts the query
-            conn.connect();
-            int response = conn.getResponseCode();
-            Log.d(DEBUG_TAG, "The response is: " + response);
-            is = conn.getInputStream();
-
-            // Convert the InputStream into a string
-            String contentAsString = readIt(is, len);
-            return contentAsString;
-
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
-        }
-        finally {
-            if (is != null) {
-                is.close();
-            }
-        }
-    }
-
-    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[len];
-        reader.read(buffer);
-        return new String(buffer);
     }
 }
